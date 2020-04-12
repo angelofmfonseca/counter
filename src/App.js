11757aae0ad4fdbce04bc6872e519c8e173b1a65
Angelo Fernandes Machado Fonseca <angelofmfonseca@gmail.com>
2020-04-12 20:49:37 -0300

    ajustes de layout

--- src/App.js
+++ src/App.js
@@ -1,35 +1,40 @@
 import React, { Component } from 'react'
+
 import './App.css'
 
 class App extends Component {
   state = {
     counter: 0
   }
+
   increment = () => { 
     this.setState({
       counter:this.state.counter + 1
     })
   }
+
   decrement = () => {
     this.setState({
       counter: this.state.counter - 1
     })
   }
+
   clear = () => {
     this.setState({
       counter: 0
     })
   }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
+      <section className="counter">
           Counter: { this.state.counter }
-          <button onClick={ this.increment }>+</button>
-          <button onClick={ this.decrement }>-</button>
-          <button onClick={ this.clear }>clear</button>
-        </header>
-      </div>
+          <div className="buttonsWrapper">
+            <button className="incrementButton" onClick={ this.increment }>+</button>
+            <button className="decrementButton" onClick={ this.decrement }>-</button>
+            <button className="clearButton" onClick={ this.clear }>clear</button>
+          </div>
+      </section>
     )
   }
 }
